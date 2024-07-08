440c0e0839bed7d5d76111d2aee958c470e4d1b2
Mui Pham <muipham64@gmail.com>
2024-07-08 07:17:53 -0400

    complete 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(untamperedArray, ...listOfElementsToRemove) {
+    let filteredArray = untamperedArray.filter((element) => {
+        for(let i = 0; i < listOfElementsToRemove.length;++i){
+            if(element === listOfElementsToRemove[i]) return false;
+        }
+        return true;
+    });
+    return filteredArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
