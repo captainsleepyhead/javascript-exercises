3a356ef882097470ef6148dabb269da1a22b9455
Mui Pham <muipham64@gmail.com>
2024-07-07 22:45:41 -0400

    Complete 02-repeatString-exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(strToRepeat, numToRepeat) {
+    let repeatedStr = "";
+    if(numToRepeat< 0) return "ERROR";
+    for(let i = 0; i < numToRepeat; ++i){
+        repeatedStr += strToRepeat;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
