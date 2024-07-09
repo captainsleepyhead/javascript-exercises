a9c32e2bc9c465a6bd9940e39b54e3fc48805349
Mui Pham <muipham64@gmail.com>
2024-07-09 01:58:54 -0400

    Complete 07-tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let preRoundedTemperature = ( temperature - 32 ) * 5/9;
+  let roundedTemperature = Math.floor((preRoundedTemperature + .05) * 10) / 10;
+  return roundedTemperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let preRoundedTemperature = temperature * 9/5 + 32
+  let roundedTemperature = Math.floor((preRoundedTemperature + .05)* 10) / 10;
+  return roundedTemperature;
 };
 
 // Do not edit below this line
