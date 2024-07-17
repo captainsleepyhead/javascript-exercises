8ee6ca4333f8214b383c8bf504c422dff912c90c
Mui Pham <muipham64@gmail.com>
2024-07-17 10:34:21 -0400

    Complete 10-fibonacci exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let lexicon = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
+    let L = 0;
+    let R = str.length-1
+    while(L<R){
+        while(!(lexicon.includes(str[L]))){
+            L++;
+        }
+        while(!(lexicon.includes(str[R]))){
+            R--;
+        }
+        if(str[L].toLowerCase()!=str[R].toLowerCase())return false;
+        L++; R--;
+    }
+    return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    a = +a;
+    if(a == 0) return 0;
+    if(a < 0) return "OOPS";
+    let arry = Array(a);
+    for(let i = 0; i < arry.length;++i){
+        if(i == 0 || i == 1){
+            arry[i] = 1;
+        }else{
+            arry[i] = arry[i-1] + arry[i-2];
+        }
+    }
+    return arry[a-1];
 
 };
 
